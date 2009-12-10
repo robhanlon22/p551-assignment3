7e24897da925846ffc2dc5063cd8c330b8274e52
Daniel Otero <oterod@dabears.(none)>
2009-12-09 19:42:09 -0800

    Base version of acceptor in place.

--- acceptor.rb
+++ acceptor.rb
@@ -1,2 +1,36 @@
-class Acceptor
+class Acceptor < PaxosRole
+  MIN_PROPOSAL_VALUE = 1
+  
+  def initialize(supervisor)
+    @supervisor = supervisor
+    @propose_mutex = Mutex.new
+    @highest_accepted = Proposal.new
+    @highest_prepare = MIN_PROPOSAL_VALUE - 1
+  end
+  
+  protected
+  
+  def prepare(proposal_number)
+    @propose_mutex.synchronize do
+      if @proposal_number > @highest_prepare
+        @highest_prepare = @proposal_number
+        return Response.new(@highest_accepted.number, self)
+      else
+        return nil
+      end
+    end
+  end
+  
+  def request_accept(proposal)
+    @propose_mutex.synchronize do
+      if proposal.number >= @highest_prepare
+        @highest_accepted = proposal
+        return true
+      end
+    end
+  end
+  
+  Response = Struct.new :highest_accepted, :acceptor
+  
+  Proposal = Struct.new :number, :value, :proposer
 end
