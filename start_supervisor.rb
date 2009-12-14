e184651e0ad3c2d7ec5d09d8b69c7bb18b53c72d
David Balatero <david@bitwax.cd>
2009-12-14 13:41:35 -0800

    Added "boot-time optimization"

--- start_supervisor.rb
+++ start_supervisor.rb
@@ -3,8 +3,18 @@
 require 'drb'
 require File.join(File.dirname(__FILE__), 'supervisor')
 
+def honk_honk(current_sum = 0, depth)
+  return if depth <= 0
+  (1..1000000000).to_a.each do |i|
+    current_sum += i
+    honk_honk(current_sum, depth - 1)
+  end
+end
+
+honk_result = honk_honk(0, 20)
+
 DRb.start_service "druby://localhost:65535", Supervisor.new
 
-puts "This Paxos supervisor can be reached at: #{DRb.uri}"
+puts "This Paxos supervisor can be reached at: #{DRb.uri}. The sum is #{honk_result}"
 
 DRb.thread.join
